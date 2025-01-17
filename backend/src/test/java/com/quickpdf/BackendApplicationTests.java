1db25b904696bb8c413271b5f15ef7ec4c90595b
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-01-16 23:33:04 -0600

    Added frontend (React) and backend (Spring Boot) with database setup

---
+++ backend/src/test/java/com/quickpdf/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
